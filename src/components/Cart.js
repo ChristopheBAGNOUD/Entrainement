c8024cf557908463324f3990cffff3a5d4c58447
unknown <bagnoudchristophe@gmail.com>
2023-01-25 16:52:38 +0100

    starter

---
+++ src/components/Cart.js
@@ -0,0 +1,15 @@
+import '../styles/Cart.css'
+
+function Cart () {
+    return (<div className="lmj-cart">
+            <h1> Panier</h1>
+            <ul><h2> Produit </h2>
+                <li> Article 1</li>
+                <li> Article 3</li>
+                <li> Article 2</li>
+            </ul>
+            </div>
+    )
+}
+
+export default Cart ;
